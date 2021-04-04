ee48c46d2f81f7598241d6f2ec8b2428747b5789
DESKTOP-D73SHNV\Serj <donserhio@tut.by>
2021-04-04 22:20:56 +0300

    Done basic project setup.

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import logo from './logo.svg'
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <img src={logo} alt="Site logotype" width="20%" height="20%"/>
+    </div>
+  )
+}
+
+export default App;
